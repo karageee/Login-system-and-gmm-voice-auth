c7a97eb8f9414d3f16f383af6eb18ae17d721eef
karageee <70268861+karageee@users.noreply.github.com>
2020-10-31 15:46:56 +0700

    flask and codes revamp

--- Program/API/Main_function.py
+++ Program/API/Main_function.py
@@ -3,6 +3,7 @@ import glob
 import os
 import sys
 import pickle
+import time
 from numpy import genfromtxt
 import pyaudio
 import wave
@@ -13,6 +14,11 @@ warnings.filterwarnings("ignore")
 
 from sklearn import preprocessing
 from python_speech_features import mfcc
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
 
 #Calculate and returns the delta of given feature vector matrix
 def calculate_delta(array):
@@ -45,3 +51,83 @@ def extract_features(audio,rate):
     #combining both mfcc features and delta
     combined = np.hstack((mfcc_feat,delta)) 
     return combined
+
+def adduser():
+  
+    source = "./voice_database/" + name #change to user ID
+  
+    os.mkdir(source)
+
+    dest =  "./gmm_models/"
+    count = 1
+
+    for path in os.listdir(source):
+        path = os.path.join(source, path)
+
+        features = np.array([])
+      
+        # reading audio files of speaker
+        (sr, audio) = read(path)
+      
+        # extract 40 dimensional MFCC & delta MFCC features
+        vector   = extract_features(audio,sr)
+
+        if features.size == 0:
+            features = vector
+        else:
+            features = np.vstack((features, vector))
+          
+        # when features of 3 files of speaker are concatenated, then do model training
+        if count == 3:    
+            gmm = mixture.GaussianMixture(n_components = 16, n_iter = 200, covariance_type='diag',n_init = 3)
+            gmm.fit(features)
+
+            # saving the trained gaussian model
+            pickle.dump(gmm, open(dest + name + '.gmm', 'wb'))
+            print(name + ' added successfully') 
+          
+            features = np.asarray(())
+            count = 0
+        count = count + 1
+
+def recognize():
+   # Voice Authentication
+   FILENAME = "./test.wav"
+
+   audio = pyaudio.PyAudio()
+
+   modelpath = "./gmm_models/"
+
+   gmm_files = [os.path.join(modelpath,fname) for fname in 
+               os.listdir(modelpath) if fname.endswith('.gmm')]
+
+   models    = [pickle.load(open(fname,'rb')) for fname in gmm_files]
+
+   speakers   = [fname.split("/")[-1].split(".gmm")[0] for fname 
+               in gmm_files]
+ 
+   #read test file
+   sr,audio = read(FILENAME)
+   
+   # extract mfcc features
+   vector = extract_features(audio,sr)
+   log_likelihood = np.zeros(len(models)) 
+
+   #checking with each model one by one
+   for i in range(len(models)):
+       gmm = models[i]         
+       scores = np.array(gmm.score(vector))
+       log_likelihood[i] = scores.sum()
+
+   pred = np.argmax(log_likelihood)
+   identity = speakers[pred]
+  
+   # if voice not recognized than terminate the process
+   if identity == 'unknown':
+           print("Not Recognized! Try again...")
+           return
+   
+   print( "Recognized as - ", identity)
+
+if(__name__ == '__main__'):
+    app.run(debug=True)
