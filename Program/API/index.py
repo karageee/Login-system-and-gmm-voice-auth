04e31b3ddbe676dd3a77d47459ab70e67b81547f
karageee <70268861+karageee@users.noreply.github.com>
2020-11-04 11:09:48 +0700

    API working

--- Program/API/index.py
+++ Program/API/index.py
@@ -8,44 +8,45 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user/database.db'
 db = SQLAlchemy(app)
 
 class UsersModel(db.Model):
-    user_id = db.Column(db.String(100), primary_key=True)
+    auth_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(100), nullable=False)
     voice_loc = db.Column(db.String(300), nullable=False)
     def __repr__(self):
         return f"Users(user_id = {user_id}, voice_loc = {voice_loc})"
 
-db.create_all()
-
-user_put_args = reqparse.RequestParser()
-user_put_args.add_argument("user_id", type=str, help="User's UserID is required", required=True)
-user_put_args.add_argument("voice_loc", type=str, help="User's voice location is required", required=True)
+user_post_args = reqparse.RequestParser()
+user_post_args.add_argument("user_id", type=str, help="User's UserID is required", required=True)
+user_post_args.add_argument("voice_loc", type=str, help="User's voice location is required", required=True)
 
 resource_fields = {
+    'auth_id' : fields.Integer,
     'user_id' : fields.String,
     'voice_loc' : fields.String
 }
 
 class Users(Resource):
     @marshal_with(resource_fields)
-    def get(self, user_id):
-        response = UsersModel.query.filter_by(user_id=user_id).first()
+    def get(self):
+        response = UsersModel.query.all()
         if not response:
             abort(404, message="Cannot find user id")
         return response
 
     @marshal_with(resource_fields)
-    def put(self, user_id):
-        args = user_put_args.parse_args()
-        response = UsersModel.query.filter_by(user_id=user_id).first()
+    def post(self):
+        args = user_post_args.parse_args()
+        response = UsersModel.query.filter_by(user_id=args['user_id']).first()
         if response:
-            abort(409, message="Video id taken...")
+            abort(409, message="User id taken...")
 
-        user = UsersModel(user_id=args['user_id'], voice_loc=args['voice_loc'])
+        i = UsersModel.query.count()
+        user = UsersModel(auth_id=i, user_id=args['user_id'], voice_loc=args['voice_loc'])
         db.session.add(user)
         db.session.commit()
         return user, 201
 
 
-api.add_resource(Users, "/user/<string:user_id>")
+api.add_resource(Users, "/user/")
 
 if __name__ == "__main__":
     app.run(debug=True)
